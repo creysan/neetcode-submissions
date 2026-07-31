7d1581fa5f56a7ac9dc6285a471cc3212e26461d
Christian Reyes <78328673+creysan@users.noreply.github.com>
2026-07-30 22:35:07 -0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        letters = {}
+        for letter in s:
+            if letter in letters:
+                letters[letter] += 1
+            else:
+                letters[letter] = 1
+        for letter in t:
+            if letter not in letters:
+                return False
+        return True
